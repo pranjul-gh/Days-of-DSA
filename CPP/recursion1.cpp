fee313c923f29e084266b2cf0c6f4ecfef28c1ff
Pranjul <pranjulmaurya.23@gmail.com>
2024-07-10 14:35:29 +0530

    10.7.24

---
+++ CPP/recursion1.cpp
@@ -0,0 +1,22 @@
+// Reverse an array using array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseArr(int i, int arr[], int n){
+    if (n/2<i){
+        return;
+    }
+    swap(arr[i], arr[n-i-1]);
+    reverseArr(i+1, arr, n);
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++) cin>>arr[i];
+    reverseArr(0, arr, n);
+    for (int i=0; i<n; i++) cout<< arr[i] << ' ';
+    return 0;
+}
